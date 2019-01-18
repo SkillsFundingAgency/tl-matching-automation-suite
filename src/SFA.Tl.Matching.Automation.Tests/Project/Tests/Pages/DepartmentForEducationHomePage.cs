a119bf31a2e7a2d970697fb9fc57b791b25687a2
Mayur Rana <mayur.rana@digital.education.gov.uk>
2019-01-18 16:02:33 +0000

    Implemented the skeleton framework

---
+++ src/SFA.Tl.Matching.Automation.Tests/Project/Tests/Pages/DepartmentForEducationHomePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SFA.Tl.Matching.Automation.Tests.Project.Framework.Helpers;
+using SFA.Tl.Matching.Automation.Tests.Project.Tests.TestSupport;
+using OpenQA.Selenium;
+
+namespace SFA.Tl.Matching.Automation.Tests.Project.Tests.Pages
+{
+    class DepartmentForEducationHomePage : BasePage
+    {
+        private static String PAGE_TITLE = "Department\r\nfor Education";
+
+        public DepartmentForEducationHomePage(IWebDriver webDriver) : base(webDriver)
+        {
+            SelfVerify();
+        }
+
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+    }
+}
