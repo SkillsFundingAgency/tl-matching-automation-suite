c44658bc87077dc23c9f7ab94af862bf0c70770c
Mayur Rana <mayur.rana@digital.education.gov.uk>
2019-12-11 15:17:51 +0000

    New tests added for returning providers within x miles

---
+++ src/SFA.Tl.Matching.Automation.Tests/Project/Tests/StepDefinitions/Providers_Within_X_MilesSteps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SFA.Tl.Matching.Automation.Tests.Project.Framework.Helpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.Tl.Matching.Automation.Tests
+{
+    [Binding]
+    public class Providers_Within_X_MilesSteps
+    {
+        [Given(@"I have entered a postcode")]
+        public void GivenIHaveEnteredAPostcode()
+        {
+            ProviderResultsHelper.AllProviders_AllSkillAreas();              
+
+        }
+     }
+}
